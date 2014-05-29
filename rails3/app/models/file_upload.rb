71f46cc4747ca2c953df43a7a1ebfcfe6eb9ef79
Ubuntu <ubuntu@ip-10-190-50-168.ec2.internal>
2014-05-29 15:23:23 +0000

    Improving file upload code a bit.

--- rails3/app/models/file_upload.rb
+++ rails3/app/models/file_upload.rb
@@ -1,14 +1,47 @@
 class FileUpload
-  def self.save( _upload )
-    @orignal_filename =  _upload['file'].original_filename
+  
+  #-------------------------------------------------------------
+  #  Make the upload directory if it doesn't already exist
+  #-------------------------------------------------------------
+  def self.mkdir
+    time = Time.now
+    @dir = File.join( Rails.configuration.upload_dir, time.year.to_s, time.strftime( '%^b' ) )
+    FileUtils.mkdir_p( @dir )
+  end
+  
+  def self.filename( _name, _num=1 )
     #-------------------------------------------------------------
-    #  Make the upload directory if it doesn't already exist
+    #  Determine the suffix of the uploaded filename
     #-------------------------------------------------------------
-    FileUtils.mkdir_p( Rails.configuration.upload_dir )
+    count = ''
+    if _num > 1
+      count = ' ' + _num.to_s
+    end
+    ext = File.extname( _name )
+    base = File.basename( _name, ext )
+    @local_filename = base + count + ext
     #-------------------------------------------------------------
     #  Build the file path
     #-------------------------------------------------------------
-    @path = File.join( Rails.configuration.upload_dir, @orignal_filename )
+    @path = File.join( @dir, @local_filename )
+    puts @local_filename
+    #-------------------------------------------------------------
+    #  Check to see if the file exists already
+    #-------------------------------------------------------------
+    if File.file?( @path )
+      _num += 1
+      filename( _name, _num )
+    end
+  end
+  
+  def self.check( _upload )
+    @original_filename = _upload['file'].original_filename
+    filename( @original_filename )
+  end
+  
+  def self.save( _upload )
+    mkdir
+    check( _upload )
     #-------------------------------------------------------------
     #  Write the file
     #-------------------------------------------------------------
