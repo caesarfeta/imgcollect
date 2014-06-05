8005dad15ce4f15bf0b0551ad6326c505d6e0d45
Ubuntu <ubuntu@ip-10-138-16-218.ec2.internal>
2014-06-05 21:33:09 +0000

    Now prefixes can be used with SparqlQuick.

--- rails3/app/helpers/sparql_queries.rb
+++ rails3/app/helpers/sparql_queries.rb
@@ -1,15 +1,23 @@
 class SparqlQueries
   
   def initialize
-    @sparql = SparqlQuick.new
+    prefixes = {
+      :hmt => "<http://www.homermultitext.org/hmt/rdf/>", 
+      :cite => "<http://www.homermultitext.org/cite/rdf/>",
+      :rdf => "<http://www.w3.org/1999/02/22-rdf-syntax-ns#>",
+      :crm => "<http://www.cidoc-crm.org/cidoc-crm/>",
+      :dc => "<http://dublincore.org/documents/dces/>",
+      :exif => "<http://www.kanzaki.com/ns/exif#>"
+    }
+    @sparql = SparqlQuick.new( 'http://localhost:8080/ds', prefixes )
   end
   
   def keywords
-    results = @sparql.select([ :s, "<http://www.w3.org/1999/02/22-rdf-syntax-ns#keyword>", :o ])
+    results = @sparql.select([ :s, "rdf:keyword", :o ])
   end
   
   def collections
-    results = @sparql.select([ :s, "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>", "<http://www.homermultitext.org/cite/rdf/ImageArchive>" ])
+    results = @sparql.select([ :s, "rdf:type", "cite:ImageArchive" ])
   end
   
 end
--- rails3/app/helpers/sparql_quick.rb
+++ rails3/app/helpers/sparql_quick.rb
@@ -1,13 +1,22 @@
 class SparqlQuick
   
+  def initialize( _endpoint, _prefixes=nil )
+    @endpoint = _endpoint
+    @prefixes = _prefixes
+    #-------------------------------------------------------------
+    #  Grab query and update handles
+    #-------------------------------------------------------------
+    @query = handle( 'query' )
+    @update = handle( 'update' )
+  end
+  
   # _triple { Array }
   def insert( _triple )
     triple = uris( _triple )
-    sparql = self.handle( 'update' )
     #-------------------------------------------------------------
     #  Insert the data
     #-------------------------------------------------------------
-    sparql.insert_data( RDF::Graph.new { |graph|
+    @update.insert_data( RDF::Graph.new { |graph|
       graph << triple
     })
   end
@@ -18,8 +27,8 @@ class SparqlQuick
     #-------------------------------------------------------------
     #  Grab a SPARQL handle and run the query
     #-------------------------------------------------------------
-    sparql = handle( 'query' )
-    query = sparql.select.where( triple )
+    query = @query.select.where( triple )
+    puts query
     #-------------------------------------------------------------
     #  Build the results object
     #-------------------------------------------------------------
@@ -36,18 +45,14 @@ class SparqlQuick
   # _type { String }
   # @return { SPARQL::Client }
   def handle( _type )
-#    return SPARQL::Client.new( Rails.configuration.sparql_endpoint+'/'+_type )
-    return SPARQL::Client.new( 'http://localhost:8080/ds'+'/'+_type )
+    return SPARQL::Client.new( File.join( @endpoint, _type ) )
   end
   
   # Build URIs
   def uris( _triple )
     triple=[]
     _triple.each do | val |
-      if val.class == ::String
-        val = uri( val )
-      end
-      triple.push( val )
+      triple.push( uri( val ) )
     end
     return triple
   end
@@ -64,15 +69,36 @@ class SparqlQuick
     #  Are you a URI or a literal?
     #-------------------------------------------------------------
     if _val.class == ::String
+      #-------------------------------------------------------------
+      #  URI with no prefix
+      #-------------------------------------------------------------
       first = _val[0]
       last = _val[-1,1]
       if first == "<" && last == ">"
-        _val[0]=''
-        _val[-1]=''
-        return RDF::URI( _val )
+        return RDF::URI( clip( _val ) )
+      end
+      #-------------------------------------------------------------
+      #  With prefix
+      #-------------------------------------------------------------
+      if @prefixes != nil
+        pre, colon, last = _val.rpartition(':')
+        pre = pre.to_sym
+        if @prefixes.has_key?( pre )
+          return uri( "<"+clip( @prefixes[ pre ] )+last+">" )
+        end
       end
     end
     return RDF::Literal( _val )
+  end
+  
+  private
+  
+  # Clip the arrows from the edges of an RDF URI string
+  def clip( _val )
+    val = String.new( _val )
+    val[0]=''
+    val[-1]=''
+    return val
   end
   
 end
