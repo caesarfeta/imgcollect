2309580eac2e8d5546c38dd9e5c603f7cd765250
caesarfeta <adamtavares@gmail.com>
2014-07-16 17:11:05 -0400

    Playing with layout.

--- rails3/public/js/site.js
+++ rails3/public/js/site.js
@@ -10,12 +10,26 @@ jQuery(window).resize( function(){
 });
 function imageFullResize() {
 	jQuery( '.image-full' ).each( function(){
-		var h = jQuery( 'img', this ).height();
-		h = ( h < 300 ) ? 300 : h;
-		jQuery( this ).height( h );
+		//------------------------------------------------------------
+		//  Set the height
+		//------------------------------------------------------------
+		var height = jQuery( 'img', this ).height();
+		height = ( height < 300 ) ? 300 : height;
+		jQuery( this ).height( height );
 		jQuery( '.metadata', this ).css({
-			'max-height': h
+			'height': height
 		});
+		//------------------------------------------------------------
+		//  Set the width
+		//------------------------------------------------------------
+		var width = jQuery( 'img', this ).width() + jQuery( '.metadata', this ).width();
+		var win_width = jQuery( window ).width();
+		if ( width > win_width ) {
+			var img_width = jQuery( 'img', this ).width();
+			jQuery( '.metadata', this ).css({
+				'width': img_width
+			});
+		}
 	});
 }
 function startSearch() {
