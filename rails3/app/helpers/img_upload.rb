177acbed997d0f44b430c242f9f8404344253985
caesarfeta <adamtavares@gmail.com>
2014-07-24 11:26:30 -0400

    images can now be uploaded by passing URL.

--- rails3/app/helpers/img_upload.rb
+++ rails3/app/helpers/img_upload.rb
@@ -1,3 +1,4 @@
+require 'open-uri'
 class ImgUpload
   
   def save( _upload )
@@ -5,17 +6,37 @@ class ImgUpload
     #  Build
     #-------------------------------------------------------------
     @uploadDir = UploadUtils.monthDir( Rails.configuration.upload_dir )
-    check( _upload )
-    #-------------------------------------------------------------
-    #  Write the file
-    #-------------------------------------------------------------
-    File.open( @uploadPath, "wb" ) { |f| 
-      f.write( _upload['file'].read ) 
-    }
+
+    case _upload['file'].class.to_s
+      #-------------------------------------------------------------
+      #  File is an upload from the users local filesystem...
+      #-------------------------------------------------------------
+      when 'ActionDispatch::Http::UploadedFile'
+        check( _upload['file'].original_filename )
+        #-------------------------------------------------------------
+        #  Write the file
+        #-------------------------------------------------------------
+        File.open( @uploadPath, "wb" ) { |f| 
+          f.write( _upload['file'].read ) 
+        }
+      #-------------------------------------------------------------
+      #  URL pointint to a file on 
+      #-------------------------------------------------------------
+      when 'String'
+        @url = _upload['file']
+        uri = URI.parse( url )
+        check( File.basename( uri.path ) )
+        #-------------------------------------------------------------
+        #  Write the file
+        #-------------------------------------------------------------
+        open( @uploadPath, 'wb' ) do |f|
+          f << open( url ).read
+        end
+    end
   end
   
   def check( _upload )
-    @original = _upload['file'].original_filename
+    @original = _upload
     path = File.join( @uploadDir, @original )
     res = UploadUtils.filename( path )
     @uploadPath = res['path']
@@ -49,3 +70,16 @@ class ImgUpload
   end
   
 end
+
+# rest = RestTest.new( 'http://localhost:3000' )
+# local = '/usr/local/imgcollect/rails3/test/fixtures/images/forest1.JPG'
+# rest.post( 'image/upload', { :file => File.new( local, 'rb' ) } )
+# remote = 'http://www.shiftedreality.com/wp-content/uploads/2010/07/sr_mandelbulb02.jpg'
+# rest.post( 'image/upload', {:file => remote })
+# 
+# {"file"=>#<ActionDispatch::Http::UploadedFile:0x48729352 @original_filename="forest1.JPG", @headers="Content-Disposition: form-data; name=\"file\"; filename=\"forest1.JPG\"\r\nContent-Type: text/plain\r\n", @tempfile=#<Tempfile:/var/folders/yp/_dkxrwzd043dnbckn8w4hr880000gn/T/RackMultipart20140724-58195-13na2mj>, @content_type="text/plain">, "controller"=>"image", "action"=>"upload"}
+# 
+# 
+# 
+# 
+# 
