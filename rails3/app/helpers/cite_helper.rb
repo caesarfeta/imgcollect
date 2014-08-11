9e0f09c0553d3dfa21fdf8bb9904077dbd7e301e
caesarfeta <adamtavares@gmail.com>
2014-08-11 11:17:13 -0400

    Working out creation and deletion method for the CITE Helper class.

--- rails3/app/helpers/cite_helper.rb
+++ rails3/app/helpers/cite_helper.rb
@@ -1,6 +1,6 @@
 require_relative "../../../../sparql_model/lib/sparql_quick.rb"
 
-# This class contains methods for converting SparqlModel collection triples into CITE collection triples.
+# This class contains methods for converting SparqlModel collection triples into CITE collection triples
 module CiteHelper
   
   @prefixes = {
@@ -11,23 +11,22 @@ module CiteHelper
     :rdfs => "<http://www.w3.org/2000/01/rdf-schema#>"
   }
   
-  # Take a SparqlModel collection and write CITE collection triples from it.
+  # Take a SparqlModel collection and write CITE collection triples from it
   # _col { Collection }
   def self.make( _col )
     
-    # Make sure you have what you need to to CITE-ify a collection.
-    check( _col )
+    # Make sure you have what you need to to CITE-ify a collection
+    self.check( _col )
     
-    # Okay let's get started.
-    # Note that this could point at a different sparql_endpoint if need be.
+    # This could point at a different sparql_endpoint if need be
     sparql = SparqlQuick.new( Rails.configuration.sparql_endpoint, @prefixes )
     
     # First things first... mark the collection as a CITE collection
-    # and add all the necessary metadata.
+    # and add all the necessary metadata
     sparql.insert([ _col.cite_urn.ltgt, "rdf:type", "cite:ImageArchive" ])
     sparql.insert([ _col.cite_urn.ltgt, "rdf:label", _col.label ])
     
-    # Now loop through the images and add their triples.
+    # Now loop through the images and add their triples
     _col.images.each_with_index do | image, i |
       cite_img = "#{_col.cite_urn}.#{i+1}".ltgt
       cite_col = _col.cite_urn.ltgt
@@ -35,21 +34,36 @@ module CiteHelper
       sparql.insert([ cite_img, "cite:belongsTo", cite_col  ])
       sparql.insert([ cite_col, "cite:possesses", cite_img  ])
     end
-    
   end
   
   # Delete CITE collection triples reference Collection.
   # _col { Collection }
   def self.delete( _col )
     
-    # Make sure you have what you need to to CITE-ify a collection.
-    check( _col )
+    # Make sure you have a CITE-ified collection
+    self.check( _col )
     
+    # This could point at a different sparql_endpoint if need be
+    sparql = SparqlQuick.new( Rails.configuration.sparql_endpoint, @prefixes )
+    cite_col = _col.cite_urn.ltgt
     
+    # Get the associated images
+    images = sparql.get_objects([ cite_col, "cite:possesses" ])
+    
+    # Delete the related images
+    images.each do | image |
+      sub = image[:o].to_s.ltgt
+      sparql.delete([ sub, :p, :o ])
+    end
+    
+    # Delete the collections
+    sparql.delete([ cite_col, :p, :o ])
   end
   
+  private
+  
   # Make sure you're dealing with a SparqlModel Collection
-  def check( _col )
+  def self.check( _col )
     if _col.class != Collection
       raise "Argument must be an instance of Collection"
     end
@@ -57,9 +71,5 @@ module CiteHelper
       raise "Collection cite_urn cannot be null"
     end
   end
-  
-  # Tests
-  #  col = Collection.new( "Insects" )
-  #  CiteHelper.make( col )
 
 end
